4b88f0f90447819fe24fbf1c99f38f1480bb23ba
mizuhosi <shin1008@gmail.com>
2020-03-14 21:57:52 +0900

    findFiles関数作成

--- src/operateDB.py
+++ src/operateDB.py
@@ -1,26 +1,39 @@
+"""
+ 1つのcsvデータ格納フォルダに対してのDB操作を請け負う
+"""
+import glob
 import os.path
 import sqlite3
 
 
-"""
- 1つのcsvデータ格納フォルダに対してのDB操作を請け負う
-"""
-
-
 class operateDB:
-    DB_NAME = "jenkinsdata.db"
-
     """
         引数で入力されたフォルダのDBと接続する
+        DB名は固定($DB_NAME)とする。データ格納フォルダ単位で処理するため問題ないはず
     """
 
     def __init__(self, dirPath):
         if not os.path.isdir(dirPath):
-            raise NotADirectoryError("Input is not Directory!")
+            raise NotADirectoryError(dirPath + " is not Directory!")
 
         self.dirPath = dirPath
-        self.dbPath = os.path.join(self.dirPath, operateDB.DB_NAME)
+
+        DB_NAME = "jenkinsdata.db"
+        self.dbPath = os.path.join(self.dirPath, DB_NAME)
         self.conn = sqlite3.connect(self.dbPath)
 
     def __del__(self):
-        self.conn.close()
+        if hasattr(self, "conn"):
+            self.conn.close()
+
+    def findFiles(self, extension):
+        if extension[0] == '.':
+            extension = extension[1:]
+        self.fileList = glob.glob("{0}\\*.{1}".format(self.dirPath, extension))
+        self.fileList = [f for f in self.fileList if os.path.basename(f)[0] != '_']
+
+    # def readFile(self):
+    #     pass
+
+    # def readCSV(self, parameter_list):
+    #     pass
--- tests/unit/test_operateDB.py
+++ tests/unit/test_operateDB.py
@@ -2,6 +2,7 @@ import os.path
 
 import pytest
 import sqlite3
+import shutil
 
 from src.operateDB import operateDB
 
@@ -16,11 +17,28 @@ def test_construct_CreateDB(tmpdir):
     assert os.path.isfile(c.dbPath)
 
 
-def test_construct_InputFile(tmpdir):
+def test_construct_InputError_01(tmpdir):
+    # 存在するファイルパスが入力された場合の例外発生チェック
+    testFilePath = os.path.join(tmpdir, "hoge.txt")
+    with open(testFilePath, mode='w'):
+        pass
+    assert os.path.isfile(testFilePath)
+
     with pytest.raises(NotADirectoryError) as excinfo:
-        c = operateDB(os.path.join(tmpdir, "hoge.txt"))  # noqa: F841
+        c = operateDB(testFilePath)  # noqa: F841
     exceptionMsg = excinfo.value.args[0]
-    assert exceptionMsg == "Input is not Directory!"
+    assert exceptionMsg == "{0} is not Directory!".format(testFilePath)
+
+
+def test_construct_InputError_02(tmpdir):
+    # 存在しないパスが入力された場合の例外発生チェック
+    testFilePath = os.path.join(tmpdir, "hoge.txt")
+    assert not os.path.exists(testFilePath)
+
+    with pytest.raises(NotADirectoryError) as excinfo:
+        c = operateDB(testFilePath)  # noqa: F841
+    exceptionMsg = excinfo.value.args[0]
+    assert exceptionMsg == "{0} is not Directory!".format(testFilePath)
 
 
 def test_destruct_deleteeDB(tmpdir):
@@ -32,3 +50,40 @@ def test_destruct_deleteeDB(tmpdir):
     assert os.path.isfile(tmpDbPath)
     with pytest.raises(sqlite3.ProgrammingError):
         tmpConn.cursor()
+
+
+def test_findFile_01(copyData):
+    # 拡張子に"."なし
+    c = operateDB(copyData)
+    c.findFiles('.csv')
+    firstPath = os.path.join(copyData, "first.csv")
+    secondPath = os.path.join(copyData, "second.csv")
+    thirdPath = os.path.join(copyData, "thirid.csv")
+    assert c.fileList.sort() == [firstPath, secondPath, thirdPath].sort()
+
+
+def test_findFile_02(copyData):
+    # 拡張子に"."あり
+    c = operateDB(copyData)
+    c.findFiles('.csv')
+    firstPath = os.path.join(copyData, "first.csv")
+    secondPath = os.path.join(copyData, "second.csv")
+    thirdPath = os.path.join(copyData, "thirid.csv")
+    assert c.fileList.sort() == [firstPath, secondPath, thirdPath].sort()
+
+
+@pytest.fixture
+def copyData(tmpdir):
+    dataPath = os.path.join(os.path.dirname(__file__), "Data")
+    tmpDataPath = os.path.join(tmpdir, "Data")
+    shutil.copytree(dataPath, tmpDataPath)
+
+    # copyが成功しているか確認
+    firstPath = os.path.join(tmpDataPath, "first.csv")
+    secondPath = os.path.join(tmpDataPath, "second.csv")
+    thirdPath = os.path.join(tmpDataPath, "third.csv")
+    assert os.path.isfile(firstPath)
+    assert os.path.isfile(secondPath)
+    assert os.path.isfile(thirdPath)
+
+    return tmpDataPath
